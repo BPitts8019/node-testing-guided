be505ff1f06f4eb831f75aa1576112676499c5c1
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-22 19:24:35 -0700

    Added tests for post and get endpoints

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,10 @@
 const superTest = require("supertest");
 const server = require("../index");
+const db = require("../data/config");
+
+beforeEach(async () => {
+   await db.seed.run();
+});
 
 test("Welcome Route", async () => {
    const res = await superTest(server).get("/");
@@ -10,4 +15,28 @@ test("Welcome Route", async () => {
    expect(res.type).toMatch(/application\/json/i);
    //Does it return the expected data?
    expect(res.body.message).toMatch(/Welcome/i);
+});
+
+test("Get list of Hobbits", async () => {
+   const res = await superTest(server)
+      .get("/hobbits");
+
+   expect(res.status).toBe(200);
+   expect(res.type).toBe("application/json");
+   expect(res.body.length).toBeGreaterThan(0);
+   expect(res.body[0]).toEqual({
+      id: 1,
+      name: "sam"
+   });
+});
+
+test("Create a Hobbit", async () => {
+   const name = "gaffer";
+   const res = await superTest(server)
+      .post("/hobbits")
+      .send({name});
+   
+   expect(res.status).toBe(201);
+   expect(res.type).toBe("application/json");
+   expect(res.body).toEqual({id: 5, name});
 });
